b3367db4fbe79f139e4c95881cf813804318a409
rakshith515 <85064252+rakshith515@users.noreply.github.com>
2023-12-28 10:38:41 +0530

    7.KMEANS ALGORITHM.py

--- 7.KMEANS ALGORITHM.py
+++ 7.KMEANS ALGORITHM.py
@@ -1,61 +1,58 @@
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.cluster import KMeans
-import sklearn.metrics as sm
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn import datasets
+import sklearn.metrics as sm
+from sklearn.mixture import GaussianMixture
+from scipy.stats import mode
 
 iris = datasets.load_iris()
-print(iris)
-print("\n IRIS DATA : ",iris.data)
-print("\n IRIS FEATURES : \n",iris.feature_names)
-print("\n IRIS TARGET : \n",iris.target)
-print("\n IRIS TARGET NAMES : \n",iris.target_names)
-
 X = pd.DataFrame(iris.data)
-print(X)
-X.columns = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width']
-y = pd.DataFrame(iris.target)
-y.columns = ['Targets']
+Y = pd.DataFrame(iris.target)
 
-plt.figure(figsize = (14,7))
+X.columns = ['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width']
+Y.columns = ['Targets']
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(14, 7))
 colormap = np.array(['red', 'lime', 'black'])
-plt.subplot(1,2,1)
-plt.scatter(X.Sepal_Length,X.Sepal_Width,c=colormap[y.Targets],s=40)
-plt.title = ("Sepal")
-colormap1 = np.array(['blue', 'yellow', 'pink'])
-plt.subplot(1,2,2)
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap1[y.Targets],s=40)
-plt.title = ("Petal")
+plt.subplot(1, 2, 1)
+plt.scatter(X.Sepal_Length, X.Sepal_Width, c=colormap[Y.Targets], s=40)
+plt.title('SEPAL')
+plt.subplot(1, 2, 2)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[Y.Targets], s=40)
+plt.title('PETAL')
 
-model1 = KMeans(n_clusters=3,random_state=100)
-model1.fit(X)
-Y_pred = model1.labels_
-print(Y_pred)
-
-from sklearn.cluster import KMeans
-from sklearn.mixture import GaussianMixture 
-
-gnm = GaussianMixture(n_components = 3,covariance_type = "full",random_state = 100)
+gnm = GaussianMixture(n_components=3, covariance_type="full", random_state=100)
 gnm.fit(X)
-y_cluster_gnm = gnm.predict(X)
-y_cluster_gnm
+labels = gnm.predict(X)
+
+kmeans = KMeans(n_clusters=3, random_state=100)
+kmeans.fit(X)
+y_pred = kmeans.labels_
+
+
+# Print the accuracy of each model
+print("Gaussian Mixtures Accuracy: ", sm.accuracy_score(Y, labels))
+print("K-means Accuracy: ", sm.accuracy_score(Y, y_pred))
+
+# Print the confusion matrix of each model
+print("\nConfusion Matrix for Gaussian Mixture Models:\n")
+print(sm.confusion_matrix(Y, labels))
+print("\nConfusion Matrix for K-means Model:\n")
+print(sm.confusion_matrix(Y, y_pred))
 
 plt.figure(figsize = (14,7))
 colormap1 = np.array(['blue', 'yellow', 'pink'])
 plt.subplot(1,3,1)
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap1[y.Targets],s=20)
+plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap1[Y.Targets],s=20)
 plt.title = ("Real Classification")
 colormap = np.array(['red', 'lime', 'black'])
 plt.subplot(1,3,2)
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[y_cluster_gnm],s=20)
+plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[labels],s=20)
 plt.title = ("GNM Classification")
 colormap1 = np.array(['blue', 'yellow', 'pink'])
 plt.subplot(1,3,3)
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap1[Y_pred],s=40)
+plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap1[y_pred],s=40)
 plt.title = ("KMEANS CLUSTERING")
-
-
-print("GNM ACCURACY")
-print("ACCURACY",sm.accuracy_score(y,y_cluster_gnm))
-print("CONFUSION MATRIX : \n",sm.confusion_matrix(y,y_cluster_gnm))
