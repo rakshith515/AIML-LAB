8a0517089510b42077e4b314d6ed832d4702bf1b
rakshith515 <85064252+rakshith515@users.noreply.github.com>
2024-01-10 18:40:18 +0530

    6. NAVIE BAYES (2).py

--- 6. NAVIE BAYES (2).py
+++ 6. NAVIE BAYES (2).py
@@ -1,56 +1,36 @@
+import numpy as np
 import pandas as pd
-from sklearn import tree
+import sklearn
+
+data=pd.read_csv("/content/diabetes.csv")
+x=data.iloc[:,[1,2,3]].values
+y=data.iloc[:,-1].values
+
+print(data.head)
+
 from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import GaussianNB
 
-data = pd.read_csv('/content/diabetes.csv')
-print("The first 5 Values of data is :\n", data.head())
-
-# obtain train data and train output
-X = data.iloc[:, :-1]
-print("\nThe First 5 values of the train data is\n", X.head())
-
-y = data.iloc[:, -1]
-print("\nThe First 5 values of train output is\n", y.head())
-
-# convert them in numbers
-le_pregnancies = LabelEncoder()
-X.Pregnancies = le_pregnancies.fit_transform(X.Pregnancies)
-
-le_glucose = LabelEncoder()
-X.Glucose = le_glucose.fit_transform(X.Glucose)
-
-le_BloodPressure = LabelEncoder()
-X.BloodPressure = le_BloodPressure.fit_transform(X.BloodPressure)
-
-le_SkinThickness = LabelEncoder()
-X.SkinThickness = le_SkinThickness.fit_transform(X.SkinThickness)
-
-le_Insulin = LabelEncoder()
-X.Insulin = le_Insulin.fit_transform(X.Insulin)
-
-le_BMI = LabelEncoder()
-X.BMI = le_BMI.fit_transform(X.BMI)
-
-le_DiabetesPedigreeFunction = LabelEncoder()
-X.DiabetesPedigreeFunction = le_DiabetesPedigreeFunction.fit_transform(X.DiabetesPedigreeFunction)
-
-le_Age = LabelEncoder()
-X.Age = le_Age.fit_transform(X.Age)
-
-
-print("\nNow the Train output is\n", X.head())
-
-le_outcome = LabelEncoder()
-y = le_outcome.fit_transform(y)
-print("\nNow the Train output is\n",y)
+le=LabelEncoder()
+x[:,0]=le.fit_transform(x[:,0])
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.20)
+xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=0.2, random_state=100)
 
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
-pred = classifier.predict(X_test)
-from sklearn.metrics import accuracy_score,confusion_matrix
-print("Accuracy is:", accuracy_score(pred, y_test))
-print("Confusion Matrix : \n", confusion_matrix(pred, y_test))
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+xtrain=sc.fit_transform(xtrain)
+xtest=sc.fit_transform(xtest)
+
+from sklearn.naive_bayes import GaussianNB
+classifier=GaussianNB()
+classifier.fit(xtrain,ytrain)
+ypred=classifier.predict(xtest)
+
+from sklearn.metrics import confusion_matrix,accuracy_score,classification_report
+cm=confusion_matrix(ytest,ypred)
+ac=accuracy_score(ytest,ypred)
+cr=classification_report(ytest,ypred)
+ac=ac*100
+print(cm)
+print(ac)
+print(cr)
