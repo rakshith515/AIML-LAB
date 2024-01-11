42693a03c12068b569e69a58e708eb7d0eaecc28
rakshith515 <85064252+rakshith515@users.noreply.github.com>
2024-01-11 11:26:02 +0530

    3. CE ALGORITHM (2).py

--- 3. CE ALGORITHM (2).py
+++ 3. CE ALGORITHM (2).py
@@ -1,26 +1,25 @@
 import csv
 
-with open("CandidateElimination.csv") as f:
+with open('/content/CandidateElimination.csv') as f:
+
     data = list(csv.reader(f))
 
     s = data[1][:-1]
     g = [['?' for i in range(len(s))] for j in range(len(s))]
-
     for i in data:
         if i[-1] == "Yes":
             for j in range(len(s)):
-                if i[j]!=s[j]:
+                if i[j] != s[j]:
                     s[j] = '?'
                     g[j][j] = '?'
-        
-        elif i[-1] == "No":
+
+        elif i[-1] =='No':
             for j in range(len(s)):
                 if i[j]!=s[j]:
                     g[j][j] = s[j]
                 else:
                     g[j][j] = '?'
-
-        print(f"iteration {data.index(i)+1} : \n")
+        print("\n steps of CE algorithm",data.index(i)+1)
         print(s)
         print(g)
 
@@ -30,6 +29,5 @@ with open("CandidateElimination.csv") as f:
             if j!='?':
                 gh.append(i)
                 break
-
-    print("final Specific hypothesis : \n",s)
-    print("final General Hypothesis : \n",gh)
+    print("\n final specific hypothesis:\n",s)
+    print("\n final general hypothesis:\n",gh)
